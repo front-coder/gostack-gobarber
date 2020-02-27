0fe4832075dd6f2e463a08728ff55061534b310f
front-coder <alex.mac.general@gmail.com>
2020-02-27 00:45:15 -0300

    Ref.: #1 - Avatar do prestador de serviço
    Adicionado a funcionalidade de carregar uma imagem para servir de avatar do prestador de serviço

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 // import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
 
 import authMidleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 /* routes.get('/', async (req, res) => {
 	const user = await User.create({
@@ -24,5 +28,7 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMidleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
