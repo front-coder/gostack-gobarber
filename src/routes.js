df8e92734e1bd3bc6dc21585588820094a97768c
front-coder <alex.mac.general@gmail.com>
2020-03-01 23:04:18 -0300

    R3f.: #1 - Listagem de appointments de um determinado provider

--- src/routes.js
+++ src/routes.js
@@ -8,6 +8,7 @@ import SessionController from './app/controllers/SessionController';
 import FileController from './app/controllers/FileController';
 import ProviderController from './app/controllers/ProviderController';
 import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
 
 import authMidleware from './app/middlewares/auth';
 
@@ -35,6 +36,8 @@ routes.get('/providers', ProviderController.index);
 
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
+
+routes.get('/schedules', ScheduleController.index);
 
 routes.post('/files', upload.single('file'), FileController.store);
 
