84fe2d2a8e4dbdb550d80e666b2244b1d0e039eb
front-coder <alex.mac.general@gmail.com>
2020-03-04 00:41:02 -0300

    Ref.: #1 - Cancelamento de agendamento e envio de e-ameil com mailtrap

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+	host: 'smtp.mailtrap.io',
+	port: 2525,
+	secure: false,
+	auth: {
+		user: 'ed24c2ca5f99d3',
+		pass: '44db2cd79d12d0',
+	},
+	default: {
+		from: 'Equipe Gobarber <noreplay@gobarber.com>',
+	},
+};
