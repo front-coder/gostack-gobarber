8a9e49e493a729fc0902834df8c19f261523c8cf
front-coder <alex.mac.general@gmail.com>
2020-03-08 22:48:58 -0300

    Finalização da API (#2)
    Ref.:  #1 - FIna da API do Gobarber  com node/express

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,21 @@
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
 // import User from './app/models/User';
 
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
+import FileController from './app/controllers/FileController';
+import ProviderController from './app/controllers/ProviderController';
+import AppointmentController from './app/controllers/AppointmentController';
+import ScheduleController from './app/controllers/ScheduleController';
+import NotificationController from './app/controllers/NotificationController';
+import AvailableController from './app/controllers/AvailableController';
 
 import authMidleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 /* routes.get('/', async (req, res) => {
 	const user = await User.create({
@@ -24,5 +33,19 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMidleware);
 
 routes.put('/users', UserController.update);
+
+routes.get('/providers', ProviderController.index);
+routes.get('/providers/:providerId/available', AvailableController.index);
+
+routes.get('/appointments', AppointmentController.index);
+routes.post('/appointments', AppointmentController.store);
+routes.delete('/appointments/:id', AppointmentController.delete);
+
+routes.get('/schedules', ScheduleController.index);
+
+routes.get('/notifications', NotificationController.index);
+routes.put('/notifications/:id', NotificationController.update);
+
+routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
